f1db4dca09da949a197766551ee9c57284b0885c
Kwonojae <rnjsdhwo1325@gmail.com>
2022-08-02 19:50:08 +0900

    work complete

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,11 +1,115 @@
-import React from 'react'
-
+import React, {useState, useEffect } from 'react'
+import { AiFillEye, AiFillGithub } from 'react-icons/ai'
+import { motion } from 'framer-motion'
+import {AppWrap } from '../../wrapper'
+import {urlFor, client } from '../../client'
 import './Work.scss'
 
 const Work = () => {
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+
+  useEffect(() => {
+    const query = '*[_type == "works"]';
+
+    client.fetch(query)
+      .then((data) => {
+        setWorks(data);
+        setFilterWork(data);
+      })
+      /*
+      deps 배열을 비우게 된다면, 컴포넌트가 처음 나타날때에만 useEffect 에 등록한 함수가 호출됩니다.
+      리액는 이펙트 함수 내부를 볼 수 없기 때문에 
+      이 이펙트 함수를 실행시켜야할지말지 결정을 스스로 못한다고 했었고,
+
+      */
+  },[])
+
+  const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y:100, opacity: 0}]);
+
+    setTimeout(() => {
+      setAnimateCard([{ y:0, opacity: 1}]);
+
+      if(item === 'All') {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)))
+      }
+    },500)
+  }
   return (
-    <div>Work</div>
+    <>
+      <h2 className='head-text'>My Creative <span>Portfolio</span> <br /> means <span> section</span></h2>
+
+      <div className='app__work-filter'>
+        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item,index)=>(
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren:0.5}}
+        className="app__work-portfolio"
+      >
+        {filterWork.map((work, index) => (
+          <div className='app__work-item app__flex' key={index}>
+            <div className='app__work-img app__flex'>
+              <img src={urlFor(work.imgUrl)} alt={work.name} />
+
+              <motion.div
+                whileHover={{ opacity: [0, 1 ]}}
+                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                className="app__work-hover app__flex"
+              >
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillEye/>
+                  </motion.div>
+                </a>                
+                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillGithub/>
+                  </motion.div>
+                </a>                
+              </motion.div>
+            </div>
+
+            <div className='app__work-content app__flex'>
+              <h4 className='bold-text'>{work.title}</h4>
+              <p className='p-text' style={{ marginTop: 10}}>{work.description}</p>
+
+              <div className='app__work-tag app__flex'>
+                <p className='p-text'>{work.tags[0]}</p>
+              </div>
+
+            </div>
+
+          </div>
+        ))}
+      </motion.div>
+    </>
   )
 }
 
-export default Work
+export default AppWrap(Work, 'work')
